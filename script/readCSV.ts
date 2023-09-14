e80c5093f7855f9055823142f87587decff8325a
megan-hong <meganhong17@gmail.com>
2023-09-14 12:04:04 -0400

    Add fake data and update script

--- script/readCSV.ts
+++ script/readCSV.ts
@@ -1,25 +1,57 @@
-import fs from 'fs';
+import * as fs from 'fs';
 import {parse} from 'csv-parse';
 
-let formattedData = {}
+export type FormattedData = {
+  [audienceId: string]: {
+    organizationId: string;
+    audienceId: string;
+    campaignId: string;
+    bids: {
+      [country: string]: {
+        country: string;
+        targetMaxBid: number;
+      };
+    };
+  };
+};
+
+let formattedData: FormattedData = {}
 
 /**
 Org ID
-Campaign ID
-Campaign Set ID
+Campaign ID -> audienceId
+Campaign Set ID -> campaign Id
 GEO
 Current Max Bid value
 Target Max Bid value (from csv file?)
  */
 
-fs.createReadStream("./migration_data.csv")
+fs.createReadStream("data/mock-campaign-data.csv")
   .pipe(parse({ delimiter: ",", from_line: 2 }))
   .on("data", function (row) {
+    console.log("row", row);
+
     const [organizationId, campaignId, campaignSetId, country, currMaxBid, targetMaxBid] = row;
-    console.log(row);
+
+    if (formattedData[campaignId]){
+      formattedData[campaignId].bids[country] = {country, targetMaxBid}
+    }
+    else{
+      formattedData[campaignId] = {
+        organizationId,
+        audienceId: campaignId,
+        campaignId: campaignSetId,
+        bids: {
+          [country]: {country, targetMaxBid}
+        }
+      }
+    }
+
+    console.log("formattedData", formattedData)
   })
   .on("end", function () {
     console.log("finished");
+    fs.writeFileSync('data/output/data.json', JSON.stringify(formattedData, null, 2) , 'utf-8');
   })
   .on("error", function (error) {
     console.log(error.message);
